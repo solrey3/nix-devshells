8f5ad51d4838b548f80864d24a49dda5d91ae3cf
Buddha Christ <budchris@solrey3.com>
2024-08-27 14:10:59 -0400

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,83 +1,53 @@
 {
-  description = "Nix Flake for cross-platform devshells";
+  description = "Development environments with devshells";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
-    in
-    {
-      devShells = forAllSystems (pkgs: {
-        python-data-science = pkgs.mkShell {
-          packages = with pkgs; [
-            python3
-            python3Packages.numpy
-            python3Packages.pandas
-            python3Packages.matplotlib
-            python3Packages.scipy
-            python3Packages.beautifulsoup4
-            python3Packages.lxml
-            python3Packages.selenium
-            python3Packages.requests
-          ];
-        };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          python-data-science = pkgs.mkShell {
+            packages = with pkgs; [
+              python3
+              python3Packages.numpy
+              python3Packages.pandas
+              python3Packages.matplotlib
+              python3Packages.scipy
+              python3Packages.beautifulsoup4
+              python3Packages.lxml
+              python3Packages.selenium
+              python3Packages.requests
+            ];
+          };
 
-        python-fasthtml = pkgs.mkShell {
-          packages = with pkgs; [
-            python3
-            python3Packages.pip
-          ];
+          python-fasthtml = pkgs.mkShell {
+            packages = with pkgs; [
+              python3
+              # Assuming FastHTML is available via pip, you might need pip to install it
+              python3Packages.pip
+            ];
 
-          shellHook = ''
-            pip install python-fasthtml
-          '';
-        };
+            shellHook = ''
+              pip install python-fasthtml
+            '';
+          };
 
-        typescript-devops = pkgs.mkShell {
-          packages = with pkgs; [
-            nodejs
-            yarn
-            # terraform
-            nodePackages.cdktf-cli
-            nodePackages.cdk8s-cli
-          ];
-        };
-
-        github-pages = pkgs.mkShell {
-          packages = with pkgs; [
-            ruby
-            bundler
-            jekyll
-          ];
-
-          shellHook = ''
-            bundle install
-          '';
-        };
-
-        kali-linux = pkgs.mkShell {
-          packages = with pkgs; [
-            nmap
-            wireshark
-            john
-            aircrack-ng
-            # hydra
-            sqlmap
-            metasploit
-            nikto
-            gobuster
-            hashcat
-            # Add more tools as needed
-          ];
-
-          shellHook = ''
-            echo "Kali Linux environment activated"
-          '';
+          typescript-devops = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs
+              yarn
+              terraform
+              cdktf
+              cdk8s
+            ];
+          };
         };
       });
-    };
 }
